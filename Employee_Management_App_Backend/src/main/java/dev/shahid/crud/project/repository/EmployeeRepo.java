5ce22307718384499b5be8452a0b04b3a6639096
Shahid6289 <shahid7044741160@gmail.com>
2024-07-04 15:02:21 +0530

    initial commit

---
+++ Employee_Management_App_Backend/src/main/java/dev/shahid/crud/project/repository/EmployeeRepo.java
@@ -0,0 +1,10 @@
+package dev.shahid.crud.project.repository;
+
+import dev.shahid.crud.project.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+
+}
